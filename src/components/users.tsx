779bf76ea34853f156839134ceee2772eb04110c
Napat Srichan <napatsrichan2001@gmail.com>
2022-12-04 11:57:38 -0800

    fix users key

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -6,11 +6,14 @@ const Users = (props: { data: User[] }) => {
   return (
     <span className="flex items-center">
       <>
-        {props.data.map((user) => (
-          <span className="mr-4 inline-block aspect-square w-12 rounded-full bg-zinc-500"></span>
+        {props.data.map((user, index) => (
+          <span
+            className="mr-4 inline-block aspect-square w-12 rounded-full bg-zinc-500"
+            key={index}
+          ></span>
         ))}
-        {props.data.map((user) => {
-          <Link href={`/users/${user.id}`}>
+        {props.data.map((user, index) => {
+          <Link key={index} href={`/users/${user.id}`}>
             <a>{user.name}</a>
           </Link>;
         })}
